6a07673d4c1cc988e05842a5acdfd4cb01f4c0da
Takashi HOSHINO <starpos@gmail.com>
2013-07-11 21:28:33 +0900

    r20130711a

--- bench.cpp
+++ bench.cpp
@@ -3,8 +3,10 @@
 #include <thread>
 #include <memory>
 #include <cinttypes>
+#include <mutex>
 
 #include "thread_util.hpp"
+#include "time.hpp"
 
 static inline void pause(void)
 {
@@ -18,7 +20,7 @@ private:
 public:
     explicit Spinlock(std::atomic_flag &lock) : lock_(lock) {
         while (lock_.test_and_set(std::memory_order_acquire)) {
-            //pause();
+            pause();
         }
     }
     ~Spinlock() noexcept {
@@ -26,20 +28,20 @@ public:
     }
 };
 
-class Worker : public cybozu::thread::Runnable
+class SpinWorker : public cybozu::thread::Runnable
 {
 private:
     std::atomic_flag &mutex_;
     uint64_t &counter_;
-    uint64_t n_;
+    const uint64_t n_;
     
 public:
-    Worker(std::atomic_flag &mutex, uint64_t &counter, uint64_t n)
+    SpinWorker(std::atomic_flag &mutex, uint64_t &counter, uint64_t n)
         : mutex_(mutex), counter_(counter), n_(n) {
     }
     void run() {
         for (uint64_t i = 0; i < n_; i++) {
-            //Spinlock lk(mutex_);
+            Spinlock lk(mutex_);
             counter_++;
         }
     }
@@ -53,16 +55,74 @@ public:
     }
 };
 
-int main()
+class MutexWorker : public cybozu::thread::Runnable
+{
+private:
+    std::mutex &mutex_;
+    uint64_t &counter_;
+    const uint64_t n_;
+public:
+    MutexWorker(std::mutex &mutex, uint64_t &counter, uint64_t n)
+        : mutex_(mutex), counter_(counter), n_(n) {
+    }
+    void run() {
+        for (uint64_t i = 0; i < n_; i++) {
+            std::lock_guard<std::mutex> lk(mutex_);
+            counter_++;
+        }
+    }
+    void operator()() noexcept override {
+        try {
+            run();
+            done();
+        } catch (...) {
+            throwErrorLater();
+        }
+    }
+};
+
+void testSpinlock(size_t nThreads, uint64_t n)
 {
     cybozu::thread::ThreadRunnerSet thSet;
     std::atomic_flag mutex = ATOMIC_FLAG_INIT;
     uint64_t counter = 0;
-    for (size_t i = 0; i < 10; i++) {
-        thSet.add(std::make_shared<Worker>(mutex, counter, 100000));
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<SpinWorker>(mutex, counter, n));
     }
+    cybozu::time::TimeStack<> ts;
+    ts.pushNow();
     thSet.start();
     thSet.join();
-    ::printf("%" PRIu64 "\n", counter);
+    ts.pushNow();
+    ::printf("Spinlock:  %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
+}
+
+void testMutexlock(size_t nThreads, uint64_t n)
+{
+    cybozu::thread::ThreadRunnerSet thSet;
+    std::mutex mutex;
+    uint64_t counter = 0;
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<MutexWorker>(mutex, counter, n));
+    }
+    cybozu::time::TimeStack<> ts;
+    ts.pushNow();
+    thSet.start();
+    thSet.join();
+    ts.pushNow();
+    ::printf("Mutexlock: %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
+}
+
+int main()
+{
+    size_t nThreads = 6;
+    size_t nCounts = 500000;
+    size_t nTrials = 5;
+    for (size_t i = 0; i < nTrials; i++) {
+        testSpinlock(nThreads, nCounts);
+    }
+    for (size_t i = 0; i < nTrials; i++) {
+        testMutexlock(nThreads, nCounts);
+    }
     return 0;
 }
