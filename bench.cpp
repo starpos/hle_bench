1a79fa5fb6450a284816f9ac06fca58a2bc3308f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2013-07-25 14:08:12 +0900

    bench: run fixed period insted fixed count.

--- bench.cpp
+++ bench.cpp
@@ -33,14 +33,14 @@ class SpinWorker : public cybozu::thread::Runnable
 private:
     std::atomic_flag &mutex_;
     uint64_t &counter_;
-    const uint64_t n_;
-    
+    const std::atomic<bool> &isEnd_;
+
 public:
-    SpinWorker(std::atomic_flag &mutex, uint64_t &counter, uint64_t n)
-        : mutex_(mutex), counter_(counter), n_(n) {
+    SpinWorker(std::atomic_flag &mutex, uint64_t &counter, const std::atomic<bool> &isEnd)
+        : mutex_(mutex), counter_(counter), isEnd_(isEnd) {
     }
     void run() {
-        for (uint64_t i = 0; i < n_; i++) {
+        while (!isEnd_.load()) {
             Spinlock lk(mutex_);
             counter_++;
         }
@@ -60,13 +60,13 @@ class MutexWorker : public cybozu::thread::Runnable
 private:
     std::mutex &mutex_;
     uint64_t &counter_;
-    const uint64_t n_;
+    std::atomic<bool> &isEnd_;
 public:
-    MutexWorker(std::mutex &mutex, uint64_t &counter, uint64_t n)
-        : mutex_(mutex), counter_(counter), n_(n) {
+    MutexWorker(std::mutex &mutex, uint64_t &counter, std::atomic<bool> &isEnd)
+        : mutex_(mutex), counter_(counter), isEnd_(isEnd) {
     }
     void run() {
-        for (uint64_t i = 0; i < n_; i++) {
+        while (!isEnd_.load()) {
             std::lock_guard<std::mutex> lk(mutex_);
             counter_++;
         }
@@ -81,33 +81,39 @@ public:
     }
 };
 
-void testSpinlock(size_t nThreads, uint64_t n)
+void testSpinlock(size_t nThreads, size_t execMs)
 {
     cybozu::thread::ThreadRunnerSet thSet;
     std::atomic_flag mutex = ATOMIC_FLAG_INIT;
     uint64_t counter = 0;
+    std::atomic<bool> isEnd(false);
     for (size_t i = 0; i < nThreads; i++) {
-        thSet.add(std::make_shared<SpinWorker>(mutex, counter, n));
+        thSet.add(std::make_shared<SpinWorker>(mutex, counter, isEnd));
     }
     cybozu::time::TimeStack<> ts;
     ts.pushNow();
     thSet.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
+    isEnd.store(true);
     thSet.join();
     ts.pushNow();
     ::printf("Spinlock:  %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
 }
 
-void testMutexlock(size_t nThreads, uint64_t n)
+void testMutexlock(size_t nThreads, size_t execMs)
 {
     cybozu::thread::ThreadRunnerSet thSet;
     std::mutex mutex;
     uint64_t counter = 0;
+    std::atomic<bool> isEnd(false);
     for (size_t i = 0; i < nThreads; i++) {
-        thSet.add(std::make_shared<MutexWorker>(mutex, counter, n));
+        thSet.add(std::make_shared<MutexWorker>(mutex, counter, isEnd));
     }
     cybozu::time::TimeStack<> ts;
     ts.pushNow();
     thSet.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
+    isEnd.store(true);
     thSet.join();
     ts.pushNow();
     ::printf("Mutexlock: %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
@@ -116,13 +122,13 @@ void testMutexlock(size_t nThreads, uint64_t n)
 int main()
 {
     size_t nThreads = 6;
-    size_t nCounts = 500000;
+    size_t execMs = 1000;
     size_t nTrials = 5;
     for (size_t i = 0; i < nTrials; i++) {
-        testSpinlock(nThreads, nCounts);
+        testSpinlock(nThreads, execMs);
     }
     for (size_t i = 0; i < nTrials; i++) {
-        testMutexlock(nThreads, nCounts);
+        testMutexlock(nThreads, execMs);
     }
     return 0;
 }
