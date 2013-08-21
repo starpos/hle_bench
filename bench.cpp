dc5aa0a70ef3b6c36d5b6fbc955e09a2e16fc455
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2013-08-21 20:36:54 +0900

    bench: add benchmark with HLE spinlock.

--- bench.cpp
+++ bench.cpp
@@ -29,6 +29,7 @@ public:
     }
 };
 
+template <int useHLE>
 class SpinlockHle
 {
 private:
@@ -36,20 +37,14 @@ private:
 public:
     explicit SpinlockHle(char &lock) : lock_(lock) {
         /* Acquire lock with lock elision */
-#if 1
-        while (__atomic_exchange_n(&lock_, 1, __ATOMIC_ACQUIRE|__ATOMIC_HLE_ACQUIRE))
-#else
-        while (__atomic_exchange_n(&lock_, 1, __ATOMIC_ACQUIRE))
-#endif
+        int flags = __ATOMIC_ACQUIRE | (useHLE ? __ATOMIC_HLE_ACQUIRE : 0);
+        while (__atomic_exchange_n(&lock_, 1, flags))
             _mm_pause(); /* Abort failed transaction */
     }
     ~SpinlockHle() noexcept {
         /* Free lock with lock elision */
-#if 1
-        __atomic_clear(&lock_, __ATOMIC_RELEASE|__ATOMIC_HLE_RELEASE);
-#else
-        __atomic_clear(&lock_, __ATOMIC_RELEASE);
-#endif
+        int flags = __ATOMIC_RELEASE | (useHLE ? __ATOMIC_HLE_RELEASE : 0);
+        __atomic_clear(&lock_, flags);
     }
 };
 
@@ -67,6 +62,57 @@ protected:
     void waitForReady() {
         while (!isReady_.load(std::memory_order_relaxed)) {
             pause();
+        }
+    }
+};
+
+class NoneWorker : public Worker
+{
+private:
+    uint64_t &counter_;
+public:
+    NoneWorker(uint64_t &counter, const std::atomic<bool> &isReady, const std::atomic<bool> &isEnd)
+        : Worker(isReady, isEnd), counter_(counter) {
+    }
+    void operator()() noexcept override {
+        try {
+            waitForReady();
+            run();
+            done();
+        } catch (...) {
+            throwErrorLater();
+        }
+    }
+private:
+    void run() {
+        while (!isEnd_.load(std::memory_order_relaxed)) {
+            counter_++;
+        }
+    }
+};
+
+class AtomicWorker : public Worker
+{
+private:
+    std::atomic<uint64_t> &counter_;
+public:
+    AtomicWorker(std::atomic<uint64_t> &counter,
+                 const std::atomic<bool> &isReady, const std::atomic<bool> &isEnd)
+        : Worker(isReady, isEnd), counter_(counter) {
+    }
+    void operator()() noexcept override {
+        try {
+            waitForReady();
+            run();
+            done();
+        } catch (...) {
+            throwErrorLater();
+        }
+    }
+private:
+    void run() {
+        while (!isEnd_.load(std::memory_order_relaxed)) {
+            counter_.fetch_add(1, std::memory_order_relaxed);
         }
     }
 };
@@ -99,6 +145,7 @@ private:
     }
 };
 
+template <int useHLE>
 class SpinHleWorker : public Worker
 {
 private:
@@ -121,7 +168,7 @@ public:
 private:
     void run() {
         while (!isEnd_.load(std::memory_order_relaxed)) {
-            SpinlockHle lk(mutex_);
+            SpinlockHle<useHLE> lk(mutex_);
             counter_++;
         }
     }
@@ -155,6 +202,100 @@ private:
     }
 };
 
+void runBench(cybozu::thread::ThreadRunnerSet &thSet,
+              std::atomic<bool> &isReady, std::atomic<bool> &isEnd,
+              cybozu::time::TimeStack<> &ts, size_t execMs)
+{
+    thSet.start();
+    ts.pushNow();
+    isReady.store(true, std::memory_order_relaxed);
+    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
+    isEnd.store(true, std::memory_order_relaxed);
+    ts.pushNow();
+    thSet.join();
+}
+
+void testNone(size_t nThreads, size_t execMs)
+{
+    cybozu::thread::ThreadRunnerSet thSet;
+    std::vector<uint64_t> counterV(nThreads, 0);
+    alignas(64) std::atomic<bool> isReady(false);
+    alignas(64) std::atomic<bool> isEnd(false);
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<NoneWorker>(counterV[i], isReady, isEnd));
+    }
+    cybozu::time::TimeStack<> ts;
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    uint64_t counter = 0;
+    for (uint64_t c : counterV) counter += c;
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("None:        %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , counter, ts.elapsedInUs(), nThreads, throughput, latency);
+}
+
+void testSpinlockN(size_t nThreads, size_t execMs)
+{
+    cybozu::thread::ThreadRunnerSet thSet;
+    alignas(64) std::atomic_flag mutex = ATOMIC_FLAG_INIT;
+    std::vector<uint64_t> counterV(nThreads, 0);
+    alignas(64) std::atomic<bool> isReady(false);
+    alignas(64) std::atomic<bool> isEnd(false);
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<SpinWorker>(mutex, counterV[i], isReady, isEnd));
+    }
+    cybozu::time::TimeStack<> ts;
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    uint64_t counter = 0;
+    for (uint64_t c : counterV) counter += c;
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("SpinlockN:   %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , counter, ts.elapsedInUs(), nThreads, throughput, latency);
+}
+
+template <int useHLE>
+void testSpinlockHleN(size_t nThreads, size_t execMs)
+{
+    cybozu::thread::ThreadRunnerSet thSet;
+    alignas(64) char mutex = 0;
+    std::vector<uint64_t> counterV(nThreads, 0);
+    alignas(64) std::atomic<bool> isReady(false);
+    alignas(64) std::atomic<bool> isEnd(false);
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<SpinHleWorker<useHLE> >(mutex, counterV[i], isReady, isEnd));
+    }
+    cybozu::time::TimeStack<> ts;
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    uint64_t counter = 0;
+    for (uint64_t c : counterV) counter += c;
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("SpinHleN(%d): %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , useHLE, counter, ts.elapsedInUs(), nThreads, throughput, latency);
+}
+
+void testAtomic(size_t nThreads, size_t execMs)
+{
+    cybozu::thread::ThreadRunnerSet thSet;
+    alignas(64) std::atomic<uint64_t> counter(0);
+    alignas(64) std::atomic<bool> isReady(false);
+    alignas(64) std::atomic<bool> isEnd(false);
+    for (size_t i = 0; i < nThreads; i++) {
+        thSet.add(std::make_shared<AtomicWorker>(counter, isReady, isEnd));
+    }
+    cybozu::time::TimeStack<> ts;
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    double throughput = counter.load() / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter.load();
+    ::printf("Atomic:      %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , counter.load(), ts.elapsedInUs(), nThreads, throughput, latency);
+}
+
 void testSpinlock(size_t nThreads, size_t execMs)
 {
     cybozu::thread::ThreadRunnerSet thSet;
@@ -166,16 +307,15 @@ void testSpinlock(size_t nThreads, size_t execMs)
         thSet.add(std::make_shared<SpinWorker>(mutex, counter, isReady, isEnd));
     }
     cybozu::time::TimeStack<> ts;
-    thSet.start();
-    ts.pushNow();
-    isReady.store(true);
-    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
-    isEnd.store(true);
-    ts.pushNow();
-    thSet.join();
-    ::printf("Spinlock:  %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("Spinlock:    %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , counter, ts.elapsedInUs(), nThreads, throughput, latency);
 }
 
+template <int useHLE>
 void testSpinlockHle(size_t nThreads, size_t execMs)
 {
     cybozu::thread::ThreadRunnerSet thSet;
@@ -184,18 +324,16 @@ void testSpinlockHle(size_t nThreads, size_t execMs)
     alignas(64) std::atomic<bool> isReady(false);
     alignas(64) std::atomic<bool> isEnd(false);
     for (size_t i = 0; i < nThreads; i++) {
-        thSet.add(std::make_shared<SpinHleWorker>(
+        thSet.add(std::make_shared<SpinHleWorker<useHLE> >(
                       mutex, std::ref(counter), isReady, isEnd));
     }
     cybozu::time::TimeStack<> ts;
-    thSet.start();
-    ts.pushNow();
-    isReady.store(true);
-    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
-    isEnd.store(true);
-    ts.pushNow();
-    thSet.join();
-    ::printf("SpinlockHle:  %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("SpinHle(%d):  %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , useHLE, counter, ts.elapsedInUs(), nThreads, throughput, latency);
 }
 
 void testMutexlock(size_t nThreads, size_t execMs)
@@ -209,30 +347,34 @@ void testMutexlock(size_t nThreads, size_t execMs)
         thSet.add(std::make_shared<MutexWorker>(mutex, counter, isReady, isEnd));
     }
     cybozu::time::TimeStack<> ts;
-    thSet.start();
-    ts.pushNow();
-    isReady.store(true);
-    std::this_thread::sleep_for(std::chrono::milliseconds(execMs));
-    isEnd.store(true);
-    ts.pushNow();
-    thSet.join();
-    ::printf("Mutexlock: %" PRIu64 " %lu us\n", counter, ts.elapsedInUs());
+    runBench(thSet, isReady, isEnd, ts, execMs);
+
+    double throughput = counter / (double)ts.elapsedInUs();
+    double latency = ts.elapsedInNs() / (double)counter;
+    ::printf("Mutexlock:   %12" PRIu64 " counts  %lu us  %zu threads  %f counts/us  %f ns/count\n"
+             , counter, ts.elapsedInUs(), nThreads, throughput, latency);
 }
 
 int main(int argc, char *argv[])
 {
-    size_t nThreads = 4;
-    if (1 < argc) nThreads = ::atoi(argv[1]);
+    //size_t nThreads = 4;
+    //if (1 < argc) nThreads = ::atoi(argv[1]);
     size_t execMs = 1000;
-    size_t nTrials = 5;
-    for (size_t i = 0; i < nTrials; i++) {
-        testSpinlock(nThreads, execMs);
-    }
-    for (size_t i = 0; i < nTrials; i++) {
-        testSpinlockHle(nThreads, execMs);
-    }
-    for (size_t i = 0; i < nTrials; i++) {
-        testMutexlock(nThreads, execMs);
+    size_t nTrials = 1;
+    for (size_t nThreads = 1; nThreads <= 4; nThreads++) {
+        for (size_t i = 0; i < nTrials; i++) {
+            testNone(nThreads, execMs);
+#if 1
+            testAtomic(nThreads, execMs);
+            testSpinlock(nThreads, execMs);
+            testSpinlockHle<0>(nThreads, execMs);
+            testSpinlockHle<1>(nThreads, execMs);
+            testMutexlock(nThreads, execMs);
+#endif
+            testSpinlockN(nThreads, execMs);
+            testSpinlockHleN<0>(nThreads, execMs);
+            testSpinlockHleN<1>(nThreads, execMs);
+        }
     }
     return 0;
 }
